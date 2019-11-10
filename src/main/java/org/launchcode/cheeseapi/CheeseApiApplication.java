c4a8cc2d1cb722326de4b393bd03aef3ea60ffb4
Vamp <vampiirecodes@gmail.com>
2019-11-10 17:43:49 -0500

    init project from spring initalizr

---
+++ src/main/java/org/launchcode/cheeseapi/CheeseApiApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheeseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/cheeseapi/CheeseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheeseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
