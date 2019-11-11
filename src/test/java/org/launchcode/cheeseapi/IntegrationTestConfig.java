0718598bba96698fb9e1ab2d24c83cd6ee27c873
Vamp <vampiirecodes@gmail.com>
2019-11-10 20:01:37 -0500

    test: IntegrationTestConfig annotation interface

---
+++ src/test/java/org/launchcode/cheeseapi/IntegrationTestConfig.java
@@ -0,0 +1,19 @@
+package org.launchcode.cheeseapi;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IntegrationTestConfig {
+}
