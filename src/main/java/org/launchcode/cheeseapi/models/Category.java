761d9c9e215e9eee4919d2c3463a15f724f154de
Vamp <vampiirecodes@gmail.com>
2019-11-11 12:20:09 -0500

    models/category: replace @Data annotation, add @Json annotations

--- src/main/java/org/launchcode/cheeseapi/models/Category.java
+++ src/main/java/org/launchcode/cheeseapi/models/Category.java
@@ -1,17 +1,27 @@
 package org.launchcode.cheeseapi.models;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
+
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode
 @Table(name = "categories")
 public class Category {
   @Id
@@ -21,4 +31,8 @@ public class Category {
   @Size(min = 3, max = 20, message = "name must be between 3 and 20 characters")
   @Pattern(regexp = "[A-Za-z ]+", message = "name must only contain alphabetic characters")
   private String name;
+
+  @JsonManagedReference
+  @OneToMany(fetch = FetchType.LAZY)
+  private List<Cheese> cheeses = new ArrayList<>();
 }
