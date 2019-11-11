14dd19e394022feb31152083948e445144bfd004
Vamp <vampiirecodes@gmail.com>
2019-11-11 12:19:36 -0500

    models/cheese: add transient categoryId for body mapping

--- src/main/java/org/launchcode/cheeseapi/models/Cheese.java
+++ src/main/java/org/launchcode/cheeseapi/models/Cheese.java
@@ -1,16 +1,28 @@
 package org.launchcode.cheeseapi.models;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.Pattern;
 
-@Data // automates boilerplate generation
-@Entity // registers POJO as a persisted Entity to be managed in Spring context
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "cheeses")
 public class Cheese {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +31,12 @@ public class Cheese {
   @Size(min = 3, max = 20, message = "name must be between 3 and 20 characters")
   @Pattern(regexp = "[A-Za-z ]+", message = "name must only contain alphabetic characters")
   private String name;
+
+  @JsonBackReference
+  @ManyToOne(optional = false, fetch = FetchType.EAGER)
+  private Category category;
+
+  @Transient
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private long categoryId;
 }
