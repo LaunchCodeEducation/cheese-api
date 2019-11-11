910409e71ebb890cff4dd7c118178f23dd05fe96
Vamp <vampiirecodes@gmail.com>
2019-11-11 16:12:58 -0500

    category: CategoryDTO, ref model to use DTO

--- src/main/java/org/launchcode/cheeseapi/models/Category.java
+++ src/main/java/org/launchcode/cheeseapi/models/Category.java
@@ -1,5 +1,6 @@
 package org.launchcode.cheeseapi.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,8 +13,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +21,15 @@ import java.util.List;
 @Setter
 @EqualsAndHashCode
 @Table(name = "categories")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Category {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Size(min = 3, max = 20, message = "name must be between 3 and 20 characters")
-  @Pattern(regexp = "[A-Za-z ]+", message = "name must only contain alphabetic characters")
   private String name;
 
   @JsonManagedReference
   @OneToMany(fetch = FetchType.LAZY)
-  private List<Cheese> cheeses = new ArrayList<>();
+  private List<Cheese> cheeses = new ArrayList<>(); // prevent NPE when not yet fetched
 }
---
+++ src/main/java/org/launchcode/cheeseapi/models/DTOs/CategoryDTO.java
@@ -0,0 +1,4 @@
+package org.launchcode.cheeseapi.models.DTOs;
+
+public class CategoryDTO extends NameEntityDTO {
+}
