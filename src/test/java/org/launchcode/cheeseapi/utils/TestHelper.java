d09fdd458bc4969cd4c704d0511de15f738f95a5
Vamp <vampiirecodes@gmail.com>
2019-11-11 21:13:04 -0500

    test/utils: TestHelper for building and persisting test Entities

---
+++ src/test/java/org/launchcode/cheeseapi/utils/TestHelper.java
@@ -0,0 +1,46 @@
+package org.launchcode.cheeseapi.utils;
+
+import org.launchcode.cheeseapi.models.Category;
+import org.launchcode.cheeseapi.models.Cheese;
+import org.launchcode.cheeseapi.models.Menu;
+import org.launchcode.cheeseapi.repositories.CategoryRepository;
+import org.launchcode.cheeseapi.repositories.CheeseRepository;
+import org.launchcode.cheeseapi.repositories.MenuRepository;
+
+public class TestHelper {
+  public static String menuName = "test menu";
+  public static String cheeseName = "test cheese";
+  public static String categoryName = "test category";
+  public static String description = "test description";
+
+  public static Cheese createCheese(Category category, CheeseRepository cheeseRepository) {
+    Cheese cheese = new Cheese();
+    cheese.setName(cheeseName);
+    cheese.setDescription(description);
+    cheese.setCategory(category);
+
+    return cheeseRepository.save(cheese);
+  }
+
+  public static Cheese createCheese(
+      CheeseRepository cheeseRepository, CategoryRepository categoryRepository
+  ) {
+    Category category = createCategory(categoryRepository);
+
+    return createCheese(category, cheeseRepository);
+  }
+
+  public static Category createCategory(CategoryRepository categoryRepository) {
+    Category category = new Category();
+    category.setName(categoryName);
+
+    return categoryRepository.save(category);
+  }
+
+  public static Menu createMenu(MenuRepository menuRepository) {
+    Menu menu = new Menu();
+    menu.setName(menuName);
+
+    return menuRepository.save(menu);
+  }
+}
